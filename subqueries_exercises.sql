ef92651cd11e377a27d571f1fd42a093c168882f
chriseverts <chris.everts21@gmail.com>
2021-06-30 12:35:02 -0500

    initial commit

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -13,31 +13,53 @@ FROM employees
 ##2. Find all the titles ever held by all current employees with the first name Aamod.
 
 
-SELECT first_name, Last_name
-FROM employees
-  WHERE emp_no = 
-    ( SELECT title
-        FROM titles
+SELECT title
+FROM titles
+  WHERE emp_no IN 
+    ( SELECT emp_no
+        FROM employees
             WHERE first_name = 'Aamod');
+            
+
 
 
 ##3. How many people in the employees table are no longer working for the company? Give the answer in a comment in your code.
 
+SELECT *
+  FROM employees
+    WHERE emp_no IN 
+      (SELECT emp_no 
+        FROM dept_emp
+          WHERE to_date NOT IN ('9999-01-01')  );
+          
+          
+          
 
 
+      
 
 
 ##4. Find all the current department managers that are female. List their names in a comment in your code.
 
-
+SELECT first_name, last_name, gender
+  FROM employees
+    WHERE emp_no IN (
+      SELECT emp_no 
+        FROM dept_manager
+          WHERE to_date >= NOW()) AND gender = 'F';
 
 
 
 
 ##5. Find all the employees who currently have a higher salary than the companies overall, historical average salary.
 
-
-
+SELECT *
+  FROM employees
+    WHERE emp_no IN (
+    SELECT emp_no
+      FROM salaries 
+          ) 
+            
 
 
 
