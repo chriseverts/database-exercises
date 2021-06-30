6c70c783a79190d4e7e8611e2791933db9c5e748
chriseverts <chris.everts21@gmail.com>
2021-06-30 09:55:38 -0500

    revision

--- where_exercises.sql
+++ where_exercises.sql
@@ -20,30 +20,31 @@ Where (first_name = 'Irena' or first_name = 'Vidya' or first_name =  'Maya') and
 
 ## 5. Find all current or previous employees whose last name starts with 'E'. Enter a comment with the number of employees whose last name starts with E.
 
-Select Distinct	last_name
+Select *
 From `employees`
 Where last_name LIKE 'e%';
 ## 40 rows
 
 ## 6. Find all current or previous employees whose last name starts or ends with 'E'. Enter a comment with the number of employees whose last name starts or ends with E. How many employees have a last name that ends with E, but does not start with E?
 
-Select DISTINCT last_name
+Select *
 From `employees`
 Where last_name LIKE ('%E') or ('E%');
 ## 132 ROWS
-Select DISTINCT last_name
+
+Select *
 From `employees`
 Where last_name LIKE ('%E'); 
 ## wtf
 
 ## 7.Find all current or previous employees employees whose last name starts and ends with 'E'. Enter a comment with the number of employees whose last name starts and ends with E. How many employees' last names end with E, regardless of whether they start with E?
 
-Select DISTINCT last_name
+Select *
 From `employees`
 Where last_name LIKE ('E%E'); 
 ## 5 Rows
 
-Select DISTINCT last_name
+Select *
 From `employees`
 Where last_name LIKE ('%E');
 ## 132 Rows
@@ -70,14 +71,14 @@ Where birth_date like ('%-12-25') and hire_date between '1990-1-1' and '1999-12-
 
 ## 11. Find all current or previous employees with a 'q' in their last name. Enter a comment with the number of records returned.
 
-Select DISTINCT last_name
+Select *
 From `employees`
 Where last_name LIKE ('%q%'); 
 ## 10 rows
 
 ## 12. Find all current or previous employees with a 'q' in their last name but not 'qu'. How many employees are found?
 
-Select DISTINCT last_name
+Select *
 From `employees`
 Where last_name LIKE ('%q%') and last_name not like ('%qu%');
 ## 3 rows
