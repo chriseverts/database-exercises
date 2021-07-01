836dfa559fd7369853278e3793a14e3e203f37a2
chriseverts <chris.everts21@gmail.com>
2021-07-01 10:30:22 -0500

    added more answers

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -53,20 +53,44 @@ SELECT first_name, last_name, gender
 
 ##5. Find all the employees who currently have a higher salary than the companies overall, historical average salary.
 
-SELECT emp_no, first_name, last_name
-  FROM employees
-    WHERE emp_no IN 
-      (SELECT emp_no 
-        FROM salaries
-        WHERE salary > 
-      	(SELECT AVG(salary)
-            FROM employees));
-            
-
+SELECT first_name, last_name, salary
+FROM employees
+JOIN salaries on salaries.emp_no = employees.emp_no
+WHERE salary > (
+	SELECT AVG(salary)
+	FROM salaries
+)
+AND to_date > curdate();
 
 
 
 
 ##6. How many current salaries are within 1 standard deviation of the current highest salary? (Hint: you can use a built in function to calculate the standard deviation.) What percentage of all salaries is this?
+
+
+SELECT max(salary)
+  FROM salaries;
+  
+SELECT stddev(salary)
+  FROM salaries;
+  
+SELECT * 
+FROM salaries
+  WHERE salary >= (
+    SELECT max(salary)
+      FROM salaries) - (SELECT stddev(salary) FROM salaries) AND to_date > curdate();  
+      
+      
+SELECT count(*) AS 'salaries below std dev', 
+  (count(*) / (SELECT count(*) FROM salaries)) * 100 AS 'percent of current salaries'
+  FROM salaries
+    WHERE salary >=
+      ((SELECT max(salary) FROM salaries) - (SELECT stddev(salary) FROM salaries)
+        AND to_date > curdate());
+ 
+  
+  
+
+
 
 
